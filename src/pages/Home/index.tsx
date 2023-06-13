c93dea20abcd5f5484dd15c0cd35176e221c5006
Vinivgm <mateusm749@gmail.com>
2023-06-13 22:23:26 +0100

    first project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,67 @@
+import { HandPalm, Play } from 'phosphor-react'
+
+import {
+  HomeContainer,
+  StartCountdownButton,
+  StopCountdownButton,
+} from './styles'
+import { useContext } from 'react'
+import { NewCycleForm } from './components/NewCycleForm'
+import { Countdonw } from './components/Countdown'
+import { FormProvider, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import Zod from 'zod'
+import { CyclesContext } from '../../contexts/CyclesContext'
+
+const newCyrcleFormValidationSchema = Zod.object({
+  task: Zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: Zod.number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos!')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos!'),
+})
+
+type NewCycleFormData = Zod.infer<typeof newCyrcleFormValidationSchema>
+
+export function Home() {
+  const { activeCycle, CreateNewCycle, InterruptCurrentCycle } =
+    useContext(CyclesContext)
+  const newCycleForm = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCyrcleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    CreateNewCycle(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+        <FormProvider {...newCycleForm}>
+          <NewCycleForm />
+        </FormProvider>
+        <Countdonw />
+        {activeCycle ? (
+          <StopCountdownButton onClick={InterruptCurrentCycle} type="button">
+            <HandPalm size={24} />
+            Parar
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
+      </form>
+    </HomeContainer>
+  )
+}
